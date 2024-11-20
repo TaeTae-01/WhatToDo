37eab9bbff26a6d11ddbd8cb87a43df2ff1620bb
TAEJIN <ctj0999@gmail.com>
2024-11-20 15:00:07 +0900

    merge 충돌 해결?

--- js/clock.js
+++ js/clock.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const clock = document.querySelector("p#clock");
 
 function getClock() {
@@ -10,17 +9,4 @@ function getClock() {
 }
 
 getClock();
-=======
-const clock = document.querySelector("p#clock");
-
-function getClock() {
-    const date = new Date();
-    const hours = String(date.getHours()).padStart(2, '0');
-    const minutes = String(date.getMinutes()).padStart(2, '0');
-    const seconds = String(date.getSeconds()).padStart(2, '0');
-    clock.innerText = `${hours}:${minutes}:${seconds}`;
-}
-
-getClock();
->>>>>>> 8801b12bb5296acae16b62357b16eff1519123ad
 setInterval(getClock, 1000);
